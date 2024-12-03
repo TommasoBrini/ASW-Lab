4c60c837fe3cb18d2c42501876f9b5836bd78e45
Tommaso <tommaso.brini@studio.unibo.it>
2024-12-03 15:32:18 +0100

    solved exercise 1 lab01

--- lab01/node01/execises/exercise-01/routes.js
+++ lab01/node01/execises/exercise-01/routes.js
@@ -1,5 +1,37 @@
 const requestHandler = (req, res) => {
-    // code here...
+    url = req.url;
+    method = req.method;
+
+    // rotta /
+    if (url === '/') {
+        res.write(`
+            <html>
+            <head><title>Enter Message</title></head>
+            <body>
+                <form action="/message" method="POST">
+                    <input type="text" name="message">
+                    <button type="submit">Send</button>
+                </form>
+            </body>
+        </html>`);
+        return res.end();
+    }
+
+    // rotta /message
+    if(url === '/message' && method === 'POST') {
+       //ricevere il messaggio e stamparlo in console
+        let body = '';
+        req.on('data', (chunk) => {
+            body += chunk;
+        });
+        req.on('end', () => {
+            const message = body.split('=')[1];
+            console.log("message: ", message);
+            res.end();
+        });
+        
+    }
+
 }
 
 exports.handler = requestHandler;
