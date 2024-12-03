891315445327284025f4332f9a7c118b903d7e6d
Tommaso <tommaso.brini@studio.unibo.it>
2024-12-03 15:11:57 +0100

    modify example 03

--- lab01/node01/examples/example-03/http-client.js
+++ lab01/node01/examples/example-03/http-client.js
@@ -3,6 +3,9 @@ const http = require('http');
 http.get('http://localhost:8080', (res) => {
     let data = '';
 
+    console.log('statusCode: ', res.statusCode);
+    console.log('headers: ', res.headers);
+
     res.on('data', (chunk) => {
         data += chunk;
         console.log('chunk: ' + chunk);
