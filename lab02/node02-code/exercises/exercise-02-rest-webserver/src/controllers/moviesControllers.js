4dc981680a854bff1198ee19df0f34788025124f
Tommaso <tommaso.brini@studio.unibo.it>
2024-12-04 01:02:26 +0100

    solved ex2 lab2

--- lab02/node02-code/exercises/exercise-02-rest-webserver/src/controllers/moviesControllers.js
+++ lab02/node02-code/exercises/exercise-02-rest-webserver/src/controllers/moviesControllers.js
@@ -1,15 +1,64 @@
 const movies = require('./movies.json');
 
+
 // create logic for returning all movies
+const getAllMovies = (req, res) => {
+    res.json(movies);
+}
 
 // create logic for returning a single movie based on the id
+const getMovie = (req, res) => {
+    let i = movies.findIndex((item) => req.params.id == item.id);
+    if (i > -1) {
+        return res.json(movies[i]);
+    } else {
+        return res.status(404).send('<h1>Movie not found</h1>');
+    }
+}
 
 // create logic for adding a new movie
+const createMovie = (req, res) => {
+    const newMovie = req.body;
+    const nItem = movies.length;
+    movies.push(newMovie);
+    if(movies.length > nItem){
+        res.status(201).json({description: 'New Movie Added'});
+    } else{
+        res.status(500).json({message: 'Internal Server Error'});
+    }
+}
 
 // create logic for updating a movie
+const updateMovie = (req, res) => {
+    const movie = req.body;
+    let i = movies.findIndex((item) => req.params.id == item.id);
+    if (i > -1) {
+        movies.splice(i, 1, movie);
+        res.status(201).json({ description: "movie updated" })
+    }
+    else {
+        res.status(404).json({ description: 'movie not found' });
+    }
+}
+
+const deleteMovie = (req, res) => {
+    let i = movies.findIndex((item) => req.params.id == item.id)
+    if (i > -1) {
+        movies.splice(i, 1);
+        res.json({ description: "movie deleted" })
+    }
+    else {
+        res.status(404).json({ description: 'movie not found' });
+    }
+}
 
 // create logic for deleting a movie
 
 module.exports = { 
     // export all the functions you created
+    getAllMovies,
+    getMovie,
+    createMovie,
+    updateMovie,
+    deleteMovie
 }
--- lab02/node02-code/exercises/exercise-02-rest-webserver/src/routes/moviesRoutes.js
+++ lab02/node02-code/exercises/exercise-02-rest-webserver/src/routes/moviesRoutes.js
@@ -3,18 +3,16 @@ const movieControllers = require('../controllers/moviesControllers')
 
 const router = express.Router();
 
-router.route('/');
 
-// handle the GET for all movies
+router.route('/')
+    .get(movieControllers.getAllMovies)  // handle the GET for all movies
+    .post(movieControllers.createMovie); // handle the POST for creating a new movie
 
-// handle the POST for creating a new movie
 
-router.route('/:id');
 
-// handle the GET for a single movie
+router.route('/:id').get(movieControllers.getMovie) // handle the GET for a single movie
+    .put(movieControllers.updateMovie) // handle the PUT for updating a single movie
+    .delete(movieControllers.deleteMovie); // handle the DELETE for a single movie
 
-// handle the DELETE for a single movie
-
-// handle the PUT for updating a single movie
 
 module.exports = router;
