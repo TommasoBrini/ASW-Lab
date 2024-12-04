6681a0d481e0b6e488d9df26097fabc1cc9cbae6
Tommaso <tommaso.brini@studio.unibo.it>
2024-12-04 23:10:54 +0100

    lab04 - ex1- create component header

---
+++ lab04/vue02-code/exercises/exercise-01/static/header.js
@@ -0,0 +1,8 @@
+const header = {
+    template: `
+        <header class="jumbotron text-center">
+            <h1 class="display-3">Welcome to the Movie Database</h1>
+            <p class="lead">Manage your favorite movies with ease</p>
+        </header>
+    `
+}
