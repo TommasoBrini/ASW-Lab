a7fcec7409da058f8906a884410971cb1c54f411
Tommaso <tommaso.brini@studio.unibo.it>
2024-12-05 13:07:37 +0100

    lab06 solved ex1

--- lab06/vue03-code/exercises/exercise-01/components/fancy-button.js
+++ lab06/vue03-code/exercises/exercise-01/components/fancy-button.js
@@ -1,1 +1,24 @@
-const FancyButton = Vue.defineComponent();
+const FancyButton = Vue.defineComponent({
+    data(){
+        return {
+            count: 0,
+        }
+    },
+    template: `
+        <button 
+            @click="increment"        
+            :id=id 
+            :class = "lastClicked ? 'bg-green-500' : 'bg-blue-500'"
+            class="my-14 px-4 py-2 text-white rounded">
+            <slot></slot>[x{{ moltiplicator }}] ( {{count}} click)
+        </button>
+    `,
+    props: {id: String, moltiplicator: Number, lastClicked: Boolean},
+    emits: ['increment'],
+    methods: {
+        increment(){
+            this.count++;
+            this.$emit('increment', this.id, this.moltiplicator);
+        }
+    }
+});
